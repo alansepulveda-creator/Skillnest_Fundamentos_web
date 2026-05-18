cf2635c8ad86fe0edc667fbbcdcfe02f18aaefeb
alansepulveda-creator <alansepulveda@liceovvh.cl>
2026-05-18 15:59:54 -0400

    done

---
+++ STACK_JS/01_introduccion_JS/static/js/script.js
@@ -0,0 +1,11 @@
+console.log(`conexion exitosa con js...`)
+
+function cambiarTexto() {
+    let titulo ="Hola, Bienvenido";
+if (document.getElementById("titulo").innerText == titulo){
+    document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
+document.getElementById("titulo").style.color = "red";
+} else{
+    document.getElementById("titulo").innerText = titulo;
+    document.getElementById("titulo").style.color="black";}
+}
