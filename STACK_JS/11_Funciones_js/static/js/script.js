4431b4ffa4c923e14cac1923d527efe1db82bda2
alansepulveda-creator <alansepulveda@liceovvh.cl>
2026-07-28 11:10:44 -0400

    ok

---
+++ STACK_JS/11_Funciones_js/static/js/script.js
@@ -0,0 +1,23 @@
+console.log("conexion exitosa")
+// Ejemplo Función simple (sin parametros)
+function saludar() {
+    alert("¡Hola, bienvenido!");
+}
+saludar();// Ejecución de una Funcion
+
+function saludar(nombre) {
+    console.log("¡Hola, " + nombre + "!");
+}
+saludar("Luis");
+saludar("Ana");
+function encontrarMaximo(a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+let numero1 = 10;
+let numero2 = 7;
+let maximo = encontrarMaximo(numero1, numero2);
+console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
