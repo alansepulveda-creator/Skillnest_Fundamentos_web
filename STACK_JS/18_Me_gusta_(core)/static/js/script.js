eeb64bfa3531ef5f129f3f33a60933c54e206ee9
alansepulveda-creator <alansepulveda@liceovvh.cl>
2026-09-02 15:35:23 -0400

    ok

--- STACK_JS/18_Me_gusta_(core)/static/js/script.js
+++ STACK_JS/18_Me_gusta_(core)/static/js/script.js
@@ -2,24 +2,24 @@ var likesRei = 0;
 var likesAsuka = 0;
 var likesShinji = 0;
 
-var textoLikesRei = document.getElementById("likesRei");
-var botonLikeRei = document.getElementById("btnRei");
+var textoLikesRei = document.querySelector("#likesRei");
+var botonLikeRei = document.querySelector("#btnRei");
 
 botonLikeRei.onclick = function () {
     likesRei = likesRei + 1;
     textoLikesRei.innerText = likesRei + " like(s)";
 };
 
-var textoLikesAsuka = document.getElementById("likesAsuka");
-var botonLikeAsuka = document.getElementById("btnAsuka");
+var textoLikesAsuka = document.querySelector("#likesAsuka");
+var botonLikeAsuka = document.querySelector("#btnAsuka");
 
 botonLikeAsuka.onclick = function () {
     likesAsuka = likesAsuka + 1;
     textoLikesAsuka.innerText = likesAsuka + " like(s)";
 };
 
-var textoLikesShinji = document.getElementById("likesShinji");
-var botonLikeShinji = document.getElementById("btnShinji");
+var textoLikesShinji = document.querySelector("#likesShinji");
+var botonLikeShinji = document.querySelector("#btnShinji");
 
 botonLikeShinji.onclick = function () {
     likesShinji = likesShinji + 1;
