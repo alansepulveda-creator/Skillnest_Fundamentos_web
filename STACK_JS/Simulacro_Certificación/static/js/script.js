afbe7f1095ee5e678d25538cab286220fcf0e4e1
alansepulveda-creator <alansepulveda@liceovvh.cl>
2026-09-04 12:26:31 -0400

    ok

--- STACK_JS/Simulacro_Certificación/static/js/script.js
+++ STACK_JS/Simulacro_Certificación/static/js/script.js
@@ -4,6 +4,16 @@ let boton2 = document.querySelector("#btnMas2")
 let carro = document.querySelector("#numeroCompra")
 let email = document.querySelector("#email")
 let boton = document.querySelector("#botonLogin")
+const imagen = document.getElementById("comidaMexicana")
+
+imagen.addEventListener("mouseover", function () {
+this.src = "static/images/comida-mexicana2.jpg";
+
+});
+
+imagen.addEventListener("mouseout", function () {
+this.src = "static/images/comida-mexicana.jpg";
+});
 
 boton1.addEventListener("click", function() {
     if (boton1 !== null) {
