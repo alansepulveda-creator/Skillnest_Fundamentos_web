267c7784b4944e7e865f18511f34abd9f7d4d38b
alansepulveda-creator <alansepulveda@liceovvh.cl>
2026-09-24 12:59:51 -0300

    ok

---
+++ STACK_JS/Simulacro_Certificación2/static/js/script.js
@@ -0,0 +1,42 @@
+let contador = 0;
+let boton1 = document.querySelector("#btnMas1")
+let boton2 = document.querySelector("#btnMas2")
+let carro = document.querySelector("#librosSeleccionados")
+let email = document.querySelector("#email")
+let boton = document.querySelector("#botonLogin")
+const imagen = document.getElementById("comidaMexicana")
+
+imagen.addEventListener("mouseover", function () {
+this.src = "static/";
+
+});
+
+imagen.addEventListener("mouseout", function () {
+this.src = "static/";
+});
+
+boton1.addEventListener("click", function() {
+    if (boton1 !== null) {
+        contador = contador + 1
+        carro.textContent = contador
+    } else {
+        console.log("no extiste el boton")
+    }
+})
+
+boton2.addEventListener("click", function() {
+    if (boton2 !== null) {
+        contador = contador + 1
+        carro.textContent = contador
+    } else {
+        console.log("no extiste el boton")
+    }
+})
+
+boton.addEventListener("click", function() {
+    if (email.value !== "") {
+        alert("Bienvenido\n" + email.value);
+    } else {
+        alert("Por favor, ingresa un correo valido.");
+    }
+});
